33e94309be173d65cef6732a9d65ffe28fa505f8
Ricardo Travez <travezvinueza@gmail.com>
2023-04-16 17:03:26 -0500

    usuari registro validado

--- app/src/main/java/com/ricardotravez/cliente/proyecto/activity/ui/ubicacion/Ubicacion.java
+++ app/src/main/java/com/ricardotravez/cliente/proyecto/activity/ui/ubicacion/Ubicacion.java
@@ -1,4 +1,70 @@
 package com.ricardotravez.cliente.proyecto.activity.ui.ubicacion;
 
-public class Ubicacion {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.ricardotravez.cliente.proyecto.R;
+
+public class Ubicacion extends  AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_ubicacion);
+
+        // Inflar el fragmento en la actividad
+//        Fragment_Ubicacion fragmentUbicacion= new Fragment_Ubicacion();
+//        getSupportFragmentManager().beginTransaction().add(R.id.nav_ubicacion, fragmentUbicacion).commit();
+
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLongitud = findViewById(R.id.txtLongitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng ecuador = new LatLng(-0.2143844,-78.4877817);
+        mMap.addMarker(new MarkerOptions().position(ecuador).title("Ecuador"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ecuador));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(String.valueOf(latLng.latitude));
+        txtLongitud.setText(String.valueOf(latLng.longitude));
+
+        mMap.clear();
+        LatLng ecuador = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(ecuador).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ecuador));
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText(String.valueOf(latLng.latitude));
+        txtLongitud.setText(String.valueOf(latLng.longitude));
+
+        mMap.clear();
+        LatLng ecuador = new LatLng(latLng.latitude,latLng.longitude);
+        mMap.addMarker(new MarkerOptions().position(ecuador).title(""));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ecuador));
+    }
 }
